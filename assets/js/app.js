24f03347ed8ea9140d47400d0143bfea311de041
Jonathan Foucher <jfoucher@gmail.com>
2011-09-06 08:18:29 +0200

    Building and pushing with rake

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,10 +15,32 @@ $(document).ready(function(){
       var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
       return text.replace(exp,"<a href='$1'>$1</a>");
     }
+/*
+    $('.highlight').each(function(i,el){
+        var codeWidth=$(this).find('code').outerWidth();
+        console.log(codeWidth);
+        $(this).width(codeWidth);
+    });
+    */
+    var origWidth=0;
+    $('.highlight').hover(
+    function(){
+        origWidth=$(this).width();
+        var codeWidth=$(this).find('code').outerWidth();
+        console.log(codeWidth);
+        if (origWidth<codeWidth){
+            $(this).animate({width:codeWidth});
+        }
+    },
+    function(){
+        $(this).animate({width:origWidth});
+    });
 
-    $.ajax('http://twitter.com/statuses/user_timeline/jfoucher.json?count=6', {
+/*
+    $.ajax('https://twitter.com/statuses/user_timeline/jfoucher.json?count=6', {
         dataType:'jsonp',
-        success:function(data){
+        success:function(data,status){
+            alert(data);
             $.each(data,function(i,el){
 
                 var date=new Date(Date.parse(el.created_at));
@@ -26,9 +48,29 @@ $(document).ready(function(){
                 $('#tweets').append('<li>'+replaceURLWithHTMLLinks(el.text)+'<span class="timeago" title="'+d+'">'+date.toLocaleDateString()+'</span></li>');
             });
             $(".timeago").timeago();
+        },
+        error:function(xhr,status,error){
+            alert(status);
+        },
+        complete:function(xhr,status){
+            alert(status);
+            
         }
     });
+*/
+    $.getJSON("http://twitter.com/statuses/user_timeline.json?screen_name=jfoucher&count=4&callback=?",
+         
+        function(data){
 
+            $.each(data, function(i,el){
+                var date=new Date(Date.parse(el.created_at));
+                var d=ISODateString(date);
+                $('#tweets').append('<li>'+replaceURLWithHTMLLinks(el.text)+'<span class="timeago" title="'+d+'">'+date.toLocaleDateString()+'</span></li>');
+
+            });
+            $(".timeago").timeago();
+
+        });
     $('a[href*=#]').click(function() {
 
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
