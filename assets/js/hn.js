addef84ca87f8b3e7486b4205fa59bd7bd1649bb
jfoucher <jfoucher@6px.eu>
2017-08-18 11:14:19 +0200

    Make hn comments use XMLHttpRequest instead of fetch

--- assets/js/hn.js
+++ assets/js/hn.js
@@ -1,7 +1,25 @@
+/*! HN comments v0.1 | (c) 2017 Jonathan FOUCHER | MIT LICENSE */
 
-// var ul = document.getElementById("comments");
-var fragment = document.createDocumentFragment();
+/*
+USAGE
+___
+Create a element with id="hnComments" in your markup. 
+It should have a data-hn-id attribute whose value is the id of 
+the post you made on hn, e.g. 
+https://news.ycombinator.com/item?id=15044479
+                                     ^^^^^^^^
+                                     THIS IS THE ID
 
+The comments will be inserted after that element.
+
+Example : 
+
+<h4 id="hnComments" style="margin-top:4em" data-hn-id="15044479">
+    Comments from <a href="https://news.ycombinator.com/item?id=15044479">HN</a>
+    <a href="https://news.ycombinator.com/item?id=15044479">Discuss</a>
+</h4>
+<script src="assets/js/hn.js></script>
+*/
 
 function displayComments(comments) {
     var ul = document.createElement('ul');
@@ -25,22 +43,27 @@ function displayComments(comments) {
     return ul;
 }
 
-if(window.fetch) {
-    fetch('https://hn.algolia.com/api/v1/items/'+hnId).then(function (response) {
-        if(response.ok) {
-            return response.json();
-          }
-          throw new Error('Network response was not ok.');
-    }).then(function(json) { 
-        console.log('got blob', json);
-        if(json.children && json.children.length) {
-            var ret = displayComments(json.children);
-            ret.className += ' top';
-            document.getElementById("comments").insertAdjacentElement('afterend', ret);
-        }
-      }).catch(function(error) {
-        console.log('There has been a problem with your fetch operation: ' + error.message);
-      });
-} else {
-    console.error('NO FETCH');
-}
+//Get element and hn post ID from markup
+var elem = document.getElementById("hnComments");
+var hnId = elem.getAttribute("data-hn-id");
+
+//Use XMLHttpRequest for compatibilty with ie
+xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', function(res){
+    console.log('XHR RESULT', res);
+    var json = JSON.parse(res.target.responseText);
+    if(json.children && json.children.length) {
+        var ret = displayComments(json.children);
+        ret.className += ' top';
+        elem.insertAdjacentElement('afterend', ret);
+    }
+});
+xhr.addEventListener('error', function(){
+        var ret = document.createElement('p');
+        ret.innerText = 'Could not load HN comments';
+        ret.className += 'hn error';
+        elem.insertAdjacentElement('afterend', ret);
+});
+xhr.open('GET', 'https://hn.algolia.com/api/v1/items/'+hnId);
+xhr.send();
